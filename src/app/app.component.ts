ae4756ac20b99deef70f8376984c1319e4ab5175
Raphael Moschetto <angular7508@caelum141-09>
2018-03-06 19:59:46 -0300

    First Class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: `
-    <h1>
-      Welcome to {{title}}!!
-    </h1>
-  `,
-  styles: []
+
+  selector:`app-root`,
+  templateUrl: "./app.component.html",
+  styles: [`
+  header
+  {
+    width: 100%;
+    text-align: center;
+  }
+  header h1
+  {
+    font-size:24px; 
+    text-transform: uppercase; 
+    letter-spacing:10px; 
+    color:white; 
+  }
+
+  section img
+  {
+    border:4px solid red;
+  }
+  
+    `]
+
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent{
+
 }
